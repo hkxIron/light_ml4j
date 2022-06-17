8edf24b695a65dcf2c4565c053c87eb17f16aa74
hukexin <hukexin@xiaomi.com>
2022-06-17 17:52:21 +0800

    init

---
+++ src/main/java/com/ml4j/data/DenseVec.java
@@ -0,0 +1,15 @@
+package com.ml4j.data;
+
+import lombok.extern.slf4j.Slf4j;
+
+/*
+ * Created by IntelliJ IDEA.
+ *
+ * Author: hukexin
+ * Date: 22-6-17
+ * Time: 下午5:42
+ */
+@Slf4j
+public class DenseVec {
+
+}
